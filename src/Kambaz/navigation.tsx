9011aa226a5809a31426b3daf0206973cbd097f2
priyamehta <mehta.priyar@northeastern.edu>
2025-02-05 20:13:39 -0500

    true commit

--- src/Kambaz/navigation.tsx
+++ src/Kambaz/navigation.tsx
@@ -17,7 +17,7 @@ export default function KambazNavigation() {
         target="_blank"
         className="list-group-item bg-black border-0 text-center"
       >
-        <img src="public/images/NEU.svg" width="75px" />
+        <img src="/images/NEU.svg" width="75px" />
         
       </a>
       <Link
